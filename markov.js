00b674c2468a02ca94f0d4a3a0151509482695c4
Tisza <logangirvin@hotmail.com>
2015-10-18 01:01:42 -0700

    Oh dear god please work...

--- markov.js
+++ markov.js
@@ -4,10 +4,77 @@
 // and . is a special character for end-line.
 function Markov(inputfile) {
 	var fs = require("fs");
-	fs.open(inputfile, "r", load);
+	fs.readFile(inputfile, 'utf8', processData);
 	
-	function load(err, fd) {
+	// array of arrays, keys are words from
+	var words = new Array();
+	
+	// datastructure used for word/count pairs
+	function Word(w, c) {
+		this.word = w;
+		this.count = c;
+	}
+	
+	// private, please ignore
+	// processes a file
+	function processData(err, data) {
 		if (err) throw err;
-		// fs.read(fd, buffer, offset, length, position, callback (err, bytesRead, buffer))
+		var lines = data.split("\n");
+		for(var i = 0; i < lines.length; i++) {
+			var line = lines[i];
+			var parts = line.split("\t");
+			var word = parts[0];
+			var tokens = parts[1].split(" ");
+			if (tokens.length > 0) {
+				words[word] = new Array();
+				for(var j = 0; j < tokens.length; j++) {
+					var token = tokens[j];
+					var p = token.split(":::");
+					var w = new Word(p[0], parseInt(p[1]));
+					words[word].push(w);
+				}
+			}
+		}
+	}
+	
+	// creates an array of markov chain's starting with the input array's 
+	// words.
+	function createLines(worda) {
+		var out = new Array();
+		for(var i = 0; i < worda.length; i++) {
+			out[i] = createLine(worda[i]);
+		}
+		return out;
+	}
+	
+	// creates a markov chain starting with word
+	function createLine(word) {
+		sent = word;
+		var n = getNext(word);
+		while(n != null) {
+			sent += " " + n;
+			n = getNext(n);
+		}
+		return sent;
+	}
+	
+	// private, gets the next word given the current word
+	function getNext(word) {
+		var w = words[word];
+		var total = 0;
+		for(var i = 0; i < w.length; i++) {
+			total += w[i].count;
+		}
+		var countTo = Math.floor(Math.random() * total);
+		for(var i = 0; i < w.length; i++) {
+			countTo -= w[i].count;
+			if (countTo <= 0) {
+				if (w[i].word == ".") {
+					return null;
+				} else {
+					return w[i].word;
+				}
+			}
+		}
 	}
 }
