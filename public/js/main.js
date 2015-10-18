3d9a39f0159de2f23cd2ecce40499a2fd63d54f3
Tisza <logangirvin@hotmail.com>
2015-10-18 02:08:56 -0700

    Removed clarifi reference causing 404 and re-organized/made the url option send.

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,21 @@
-function submitLink() {
-
-}
-
 (function() {
 	window.addEventListener("load", function() {
 		// adding multiple tag lines....
 		var tagline = new Array();
 		tagline.push("Give us an image and well turn it into feels");
-		document.getElementById("tagline").innerHTML = tagline[Math.floor(tagline.length * Math.random())];
-	})
+		tagline.push("Your poetry is too mainstream to belong here");
+		document.getElementById("tagline").innerHTML = 
+			tagline[Math.floor(tagline.length * Math.random())];
+		
+		$("#upload-url").on("click", function() {
+			$.ajax("/api/poems", 
+			{
+				"data" : $("#paste-url").val(),
+				"method" : "GET"
+			});
+		});
+		
+	});
+	
+	
 })();
