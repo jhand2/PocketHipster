5f06fb381d640a1f1856cf9218f43d04ad7b49bf
Tisza <logangirvin@hotmail.com>
2015-10-17 23:02:38 -0700

    Added dynamic taglines...

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 function submitLink() {
 
 }
+
+(function() {
+	window.addEventListener("load", function() {
+		// adding multiple tag lines....
+		var tagline = new Array();
+		tagline.push("Give us an image and well turn it into feels");
+		document.getElementById("tagline").innerHTML = tagline[Math.floor(tagline.length * Math.random())];
+	})
+})();
