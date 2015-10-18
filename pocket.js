97ce6f750d45a073ff1bc901df00c2ea8643b8e8
Katrina Ezis <katrinaezis@gmail.com>
2015-10-17 20:36:48 -0700

    updated

---
+++ pocket.js
@@ -0,0 +1,10 @@
+$(document).ready(
+  function(){
+      var clarifai = new Clarifai(
+          {
+            'clientId': 'YOUR_CLIENT_ID',
+            'clientSecret': 'YOUR_CLIENT_SECRET' 
+          }
+      );
+  }  
+);
