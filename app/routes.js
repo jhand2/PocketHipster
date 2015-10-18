e94dc1691ab3bfea13f806dc81777d8882da0c16
Jordan Hand <jordanhand@comcast.net>
2015-10-18 03:24:40 -0700

    added post api call

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,16 @@
 module.exports = function(app) {
+
+	var Markov = require('Markov');
+
 	app.post('/api/poems', function(req, res) {
-		
+
+		var markov = Markov(url("../public/res/naughtyHarry.dat"));
+
+		var sentence = markov.createLine("wand");
+
+		console.log(sentence);
+
+		res.json();
 	});
 
 	app.get('/', function(req, res) {
